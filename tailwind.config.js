aaef01086736f596e02035f24516980d7b0b4008
programmerNizam <mdnizam12109@gmail.com>
2023-10-09 00:43:49 +0600

    Install DaisyUi for project and setup completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2c6f8c",
+          "secondary": "#edccff",
+          "accent": "#ffc4ce",
+          "neutral": "#2a2432",
+          "base-100": "#ffffff",
+          "info": "#90dae9",
+          "success": "#47d7cb",
+          "warning": "#a35e0f",
+          "error": "#fa2e4d",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
