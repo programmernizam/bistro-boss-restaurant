9d38df8713e21db4b098d30c0cd43ce18b0680eb
programmerNizam <mdnizam12109@gmail.com>
2023-10-11 20:01:55 +0600

    Featured product section completed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,18 @@
-const ProductCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+/* eslint-disable react/prop-types */
+const ProductCard = ({ product }) => {
+  const { name, recipe, image } = product;
+  return (
+    <div className="bg-gray-200/50">
+      <img src={image} alt="product" className="w-full" />
+      <div className="card_content p-5">
+        <h3 className="text-2xl font-semibold">{name}</h3>
+        <p className="my-3">{recipe.slice(0, 70)}...</p>
+        <button className="bg-[#E8E8E8] py-3 px-8 font-semibold uppercase text-[#BB8506] rounded-lg border-b-4 border-[#BB8506] hover:bg-neutral hover:border-neutral duration-500">
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/pages/Home/FeaturedProduct/FeaturedProduct.jsx
+++ src/pages/Home/FeaturedProduct/FeaturedProduct.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../../../components/ProductCard";
 import SharedTitle from "../../../components/SharedTitle";
 import Container from "../../Shared/Container";
 
 const FeaturedProduct = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItem = data.filter((item) => item.category === "offered");
+        setProducts(popularItem);
+      });
+  }, []);
   return (
     <Container>
       <div className="text-center py-20">
@@ -9,6 +20,13 @@ const FeaturedProduct = () => {
           heading={"CHEF RECOMMENDS"}
           subheading={"---Should Try---"}
         />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+          {products
+            .map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))
+            .slice(0, 3)}
+        </div>
       </div>
     </Container>
   );
