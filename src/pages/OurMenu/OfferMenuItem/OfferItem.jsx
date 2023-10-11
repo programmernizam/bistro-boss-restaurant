4add5aaf658e7f2c3b0ae081ddaa1ddc400f17dd
programmerNizam <mdnizam12109@gmail.com>
2023-10-12 00:28:25 +0600

    Littel update on offered menu section

--- src/pages/OurMenu/OfferMenuItem/OfferItem.jsx
+++ src/pages/OurMenu/OfferMenuItem/OfferItem.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import MenuItemCard from "../../../components/MenuItemCardList";
+import SharedButton from "../../../components/SharedButton";
 import SharedTitle from "../../../components/SharedTitle";
 import Container from "../../Shared/Container";
 
@@ -20,11 +21,12 @@ const OfferItem = () => {
           subheading={"---Don't miss---"}
           heading={"TODAY'S OFFER"}
         />
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-10">
           {menu.map((item) => (
             <MenuItemCard key={item._id} item={item} />
           ))}
         </div>
+        <SharedButton title={"ORDER YOUR FAVORITE FOOD"} />
       </section>
     </Container>
   );
