22af95cfd368c78d7384672c96292e5ea8d6d097
programmerNizam <mdnizam12109@gmail.com>
2023-10-11 18:16:48 +0600

    Make a common banner component

---
+++ src/components/CommonBanner.jsx
@@ -0,0 +1,15 @@
+const CommonBanner = ({ bannerBg, heading, subheading }) => {
+  return (
+    <section
+      className="text-center p-32 bg-center bg-cover"
+      style={{ backgroundImage: `url(${bannerBg})` }}
+    >
+      <div className="bg-white p-20">
+        <h2 className="text-5xl mb-5">{heading}</h2>
+        <p>{subheading}</p>
+      </div>
+    </section>
+  );
+};
+
+export default CommonBanner;
--- src/pages/Home/FeaturedBanner/FeaturedBanner.jsx
+++ src/pages/Home/FeaturedBanner/FeaturedBanner.jsx
@@ -1,24 +1,14 @@
 import banner from "../../../assets/home/chef-service.jpg";
+import CommonBanner from "../../../components/CommonBanner";
 import Container from "../../Shared/Container";
 const FeaturedBanner = () => {
   return (
     <Container>
-      <section
-        className="text-center p-32 bg-center bg-cover"
-        style={{ backgroundImage: `url(${banner})` }}
-      >
-        <div className="bg-white p-20">
-          <h2 className="text-5xl mb-5">Bistro Boss</h2>
-          <p>
-            Bistro Boss is a culinary haven where sophistication meets
-            indulgence. A cozy ambiance beckons diners into a world of exquisite
-            flavors and impeccable service. With a diverse menu and top-notch
-            ingredients, every visit promises a delightful gastronomic affair,
-            leaving patrons captivated and eager for their next culinary
-            escapade.
-          </p>
-        </div>
-      </section>
+      <CommonBanner
+        heading={"Bistro Boss"}
+        bannerBg={banner}
+        subheading={`Bistro Boss is a culinary haven where sophistication meets indulgence.A cozy ambiance beckons diners into a world of exquisite flavors and impeccable service. With a diverse menu and top-notch ingredients, every visit promises a delightful gastronomic affair, leaving patrons captivated and eager for their next culinary escapade.`}
+      />
     </Container>
   );
 };
