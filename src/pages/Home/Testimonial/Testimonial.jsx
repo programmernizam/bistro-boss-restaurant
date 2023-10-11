e9044904f0020a5a87f8039c6d2f36020d68406e
programmerNizam <mdnizam12109@gmail.com>
2023-10-11 22:11:18 +0600

    Home page complete without responsive

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,20 +1,21 @@
 import SharedTitle from "../../../components/SharedTitle";
 import Container from "../../Shared/Container";
 
+import { BiSolidQuoteLeft } from "react-icons/bi";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
+import "@smastrom/react-rating/style.css";
 import "swiper/css";
 import "swiper/css/pagination";
-
 // import required modules
+import { Rating } from "@smastrom/react-rating";
 import { useEffect, useState } from "react";
 import { Pagination } from "swiper/modules";
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
     fetch("reviews.json")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
   return (
@@ -26,13 +27,26 @@ const Testimonial = () => {
         />
         <div>
           <Swiper
+            slidesPerView={1}
             pagination={true}
             navigation={true}
             modules={[Pagination]}
             className="mySwiper"
           >
             {reviews.map((review) => (
-              <SwiperSlide key={review._id}></SwiperSlide>
+              <SwiperSlide key={review._id}>
+                <div className="text-center mt-10 pb-10">
+                  <Rating
+                    style={{ maxWidth: 180 }}
+                    value={review.rating}
+                    readOnly
+                    className="mx-auto"
+                  />
+                  <BiSolidQuoteLeft className="text-8xl mx-auto" />
+                  <p>{review.details}</p>
+                  <h4 className="text-xl font-semibold text-secondary">{review.name}</h4>
+                </div>
+              </SwiperSlide>
             ))}
           </Swiper>
         </div>
