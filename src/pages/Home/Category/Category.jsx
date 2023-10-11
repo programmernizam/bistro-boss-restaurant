803221360ee9073846d4799440e31e28ebf9ee23
programmerNizam <mdnizam12109@gmail.com>
2023-10-11 13:27:55 +0600

    Home page category section slider completed

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,3 +1,5 @@
+import CategorySlider from "./CategorySlider";
+
 const Category = () => {
   return (
     <section className="py-20 text-center">
@@ -7,6 +9,7 @@ const Category = () => {
       <h3 className="text-[40px] border-y-4 inline-block px-10 mt-5 border-[#E8E8E8]">
         ORDER ONLINE
       </h3>
+      <CategorySlider />
     </section>
   );
 };
---
+++ src/pages/Home/Category/CategorySlider.jsx
@@ -0,0 +1,69 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+// import required images
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+export default function CategorySlider() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        spaceBetween={24}
+        autoplay={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mt-10 max-w-screen-xl pb-16"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="slide" className="" />
+          <h3 className="text-4xl text-white -mt-16 uppercase pb-5">Salads</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="slide" className="" />
+          <h3 className="text-4xl text-white -mt-16 uppercase pb-5">Pizza</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="slide" className="" />
+          <h3 className="text-4xl text-white -mt-16 uppercase pb-5">Soups</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="slide" className="" />
+          <h3 className="text-4xl text-white -mt-16 uppercase pb-5">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="slide" className="" />
+          <h3 className="text-4xl text-white -mt-16 uppercase pb-5">Salads</h3>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
