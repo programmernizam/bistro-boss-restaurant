4a7cc87e3cd7a5a2dba8dce8d58861359de9070b
programmerNizam <mdnizam12109@gmail.com>
2023-10-12 14:00:09 +0600

    React Helmet added

--- src/pages/ContactUs/Contact/Contact.jsx
+++ src/pages/ContactUs/Contact/Contact.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Contact = () => {
   return (
     <section>
+      <Helmet>
+        <title>Contact Us - Bistro Boss</title>
+      </Helmet>
       <h2>This is contact page</h2>
     </section>
   );
--- src/pages/Shop/Shop/Shop.jsx
+++ src/pages/Shop/Shop/Shop.jsx
@@ -1,9 +1,13 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Hero/Banner";
 import Product from "../Product/Product";
 
 const Shop = () => {
   return (
     <section>
+      <Helmet>
+        <title>Our Shop - Bistro Boss</title>
+      </Helmet>
       <Banner />
       <Product />
     </section>
