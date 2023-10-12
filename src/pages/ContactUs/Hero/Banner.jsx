8aa6b495d7a4ce89d6743a647c4f50b196c3f6b9
programmerNizam <mdnizam12109@gmail.com>
2023-10-13 00:01:51 +0600

    Contact page banner section create

--- src/pages/ContactUs/Hero/Banner.jsx
+++ src/pages/ContactUs/Hero/Banner.jsx
@@ -1,9 +1,16 @@
+import banner from "../../../assets/contact/banner.jpg";
+import CommonBanner from "../../../components/CommonBanner";
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="-mt-[90px]">
+      <CommonBanner
+        containerBg={"bg-black/60 text-white"}
+        bannerBg={banner}
+        heading={"CONTACT US"}
+        subheading={"Would you like to try a dish?"}
+      />
+    </div>
+  );
 };
 
 export default Banner;
