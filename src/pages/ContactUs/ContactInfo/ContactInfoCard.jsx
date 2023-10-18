1208d27561041732761b3c421cac9d330f72cd05
programmerNizam <mdnizam12109@gmail.com>
2023-10-18 15:29:37 +0600

    Contact page top info card completed to design

--- src/pages/ContactUs/ContactInfo/ContactInfoCard.jsx
+++ src/pages/ContactUs/ContactInfo/ContactInfoCard.jsx
@@ -2,8 +2,8 @@ const ContactInfoCard = ({ icon, title, description }) => {
   return (
     <div className="bg-white border-gray-100 border-[1px]">
       <div className="flex justify-center bg-secondary p-5">{icon}</div>
-      <div className="px-5 py-20">
-        <h3 className="text-xl">{title}</h3>
+      <div className="px-5 py-14 bg-gray-100 m-5">
+        <h3 className="text-2xl font-semibold">{title}</h3>
         <p>{description}</p>
       </div>
     </div>
