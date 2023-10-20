f0451b66d978619c4e50508992467c513a2d1221
programmerNizam <mdnizam12109@gmail.com>
2023-10-20 15:07:28 +0600

    Contact form comontent start building

---
+++ src/pages/ContactUs/ContactForm/ContactForm.jsx
@@ -0,0 +1,14 @@
+import SharedTitle from "../../../components/SharedTitle";
+import Container from "../../Shared/Container";
+
+const ContactForm = () => {
+  return (
+    <Container>
+      <div>
+        <SharedTitle heading={""} subheading={""} />
+      </div>
+    </Container>
+  );
+};
+
+export default ContactForm;
