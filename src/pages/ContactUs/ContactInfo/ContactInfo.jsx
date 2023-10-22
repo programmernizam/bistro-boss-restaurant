300ca7649e9ccffa916fdbfbad9714bd55cb44a3
programmerNizam <mdnizam12109@gmail.com>
2023-10-22 21:11:21 +0600

    Contact info  update

--- src/pages/ContactUs/ContactInfo/ContactInfo.jsx
+++ src/pages/ContactUs/ContactInfo/ContactInfo.jsx
@@ -10,17 +10,17 @@ const ContactInfo = () => {
         <SharedTitle heading={"OUR LOCATION"} subheading={"---Visit Us---"} />
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
           <ContactInfoCard
-            icon={<FaPhoneVolume className="text-2xl text-white" />}
+            icon={<FaPhoneVolume className="text-3xl text-white" />}
             title={"Phone"}
             description={"+880 1234567890"}
           />
           <ContactInfoCard
-            icon={<FaLocationDot className="text-2xl text-white" />}
+            icon={<FaLocationDot className="text-3xl text-white" />}
             title={"Address"}
             description={"123 ABS Street, Uni 21, Bangladesh"}
           />
           <ContactInfoCard
-            icon={<FaClock className="text-2xl text-white" />}
+            icon={<FaClock className="text-3xl text-white" />}
             title={"Working Hours"}
             description={"Sun-Fri: 08:00 - 22:00"}
           />
