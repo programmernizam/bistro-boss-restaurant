46509023210083826a4af2f89e09810677a0273a
programmerNizam <mdnizam12109@gmail.com>
2023-11-05 15:07:15 +0600

    Home page menu item section make responsive

--- src/components/MenuItemCardList.jsx
+++ src/components/MenuItemCardList.jsx
@@ -1,18 +1,18 @@
-
 const MenuItemCard = ({ item }) => {
   const { name, recipe, image, price } = item;
   return (
-    <div className="flex gap-8">
+    <div className="flex flex-col items-center p-5 md:items-start md:p-0 md:flex-row gap-8">
       <img
         src={image}
         alt="menuitems"
-        className="w-[110px]"
-        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-full md:w-[110px] md:rounded-tl-none md:rounded-r-[200px] md:rounded-l-[200px]"
       />
       <div className="w-full">
         <div className="flex justify-between items-start gap-5">
           <div className="text-start">
-            <h3 className="text-xl font-semibold">{name} ------------------</h3>
+            <h3 className="text-xl font-semibold">
+              {name} <span className="hidden md:inline-block">------------------</span>
+            </h3>
             <p>{recipe}</p>
           </div>
           <h3 className="text-xl text-secondary font-bold text-right">
