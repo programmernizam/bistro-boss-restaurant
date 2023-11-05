b6e90c850c9a5d634bf671d78d0cf8e121459e1b
programmerNizam <mdnizam12109@gmail.com>
2023-11-05 15:09:17 +0600

    Shared heading make responsive

--- src/components/SharedTitle.jsx
+++ src/components/SharedTitle.jsx
@@ -3,7 +3,7 @@ const SharedTitle = ({ subheading, heading }) => {
   return (
     <div>
       <p className="text-secondary font-bold italic text-xl">{subheading}</p>
-      <h3 className="text-[40px] border-y-4 inline-block px-10 mt-5 border-[#E8E8E8]">
+      <h3 className="text-3xl md:text-[40px] border-y-4 inline-block px-10 mt-5 border-[#E8E8E8]">
         {heading}
       </h3>
     </div>
