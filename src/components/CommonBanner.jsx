d6360797cbbaa4f3a2acaff4dfd4f91aae26509a
programmerNizam <mdnizam12109@gmail.com>
2023-11-05 14:54:51 +0600

    Home page featured section make responsive work

--- src/components/CommonBanner.jsx
+++ src/components/CommonBanner.jsx
@@ -1,15 +1,14 @@
 import Container from "../pages/Shared/Container";
 
-
 const CommonBanner = ({ bannerBg, heading, subheading, containerBg }) => {
   return (
     <section
-      className="text-center p-32 bg-center bg-cover"
+      className="text-center p-8 md:p-32 bg-center bg-cover"
       style={{ backgroundImage: `url(${bannerBg})` }}
     >
       <Container>
-        <div className={`p-20 ${containerBg}`}>
-          <h2 className="text-5xl mb-5">{heading}</h2>
+        <div className={`p-5 md:p-10 lg:p-20 ${containerBg}`}>
+          <h2 className="text-3xl md:text-5xl mb-5">{heading}</h2>
           <p>{subheading}</p>
         </div>
       </Container>
